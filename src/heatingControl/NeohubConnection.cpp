5d6cd0109dffab415c9a49f02c35e7ebb75c5c79
Christof Edel <christof@edel.aero>
2025-11-27 00:54:12 +0000

    Working Neohub connection

--- src/heatingControl/NeohubConnection.cpp
+++ src/heatingControl/NeohubConnection.cpp
@@ -1,76 +1,169 @@
 #include "NeohubCOnnection.h"
 
-NeohubConnection::NeohubConnection(const String &hubUrl) {
-    m_url = hubUrl;
-    if (!m_url.startsWith("ws:")) m_url = "wss:/" + m_url;
-    if (!m_url.endsWith("/")) m_url += ":4243/"; 
+
+NeohubConnection::NeohubConnection(const String &hubHost) {
+    this->m_host = hubHost;
+}
+NeohubConnection::~NeohubConnection() {
 }
 
-NeohubConnectionResult NeohubConnection::open() {
+bool NeohubConnection::open(int timeoutMillis /* = -1 */) {
 
-    // Configure the client for access without certificate checking
-    // (beause the hub provides a self-signed certificate)
-    esp_websocket_client_config_t cfg = {};
-    cfg.uri = this->m_url.c_str();
-    cfg.skip_cert_common_name_check = true;
-    cfg.cert_pem = nullptr;            // accept any cert (insecure)
-    cfg.disable_auto_reconnect = true; // just one shot
-
-    // Initialise the client and register our event handler
-    this->m_websocketClient = esp_websocket_client_init(&cfg);
-    if (!this->m_websocketClient) return NeohubConnectionResult::UnableToInitialiseClient;
-
-    esp_websocket_register_events(this->m_websocketClient, WEBSOCKET_EVENT_ANY, NeohubConnection::websocketEventHandler, this);
-    esp_websocket_client_start(this->m_websocketClient);
-
-    return NeohubConnectionResult::Ok;
+    this->m_websocketClient.beginSSL(this->m_host, 4243, "/");
+    this->m_websocketClient.onEvent(NeohubConnection::webSocketEventHandler, this);
+    this->addToLoopTask();
+    ensureLoopTask();
+    return true;
 }
 
 bool NeohubConnection::send(String json, int timeoutMillis /* = -1 */) {
-    // Upstream WS is connected → send the request payload
-    int result = esp_websocket_client_send_text(
-        this->m_websocketClient,
-        json.c_str(),
-        json.length(),
-        timeoutMillis <= 0 ? portMAX_DELAY : pdMS_TO_TICKS(timeoutMillis)
-    );
-    return result >= 0;
-    // On error, result is -1 and actual errors are reported using the onerror callback
+    return this->m_websocketClient.sendTXT(json);
 }
 
-void NeohubConnection::websocketEventHandler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
-    NeohubConnection* _this = (NeohubConnection *) arg;
-    switch (event_id) {
-        case WEBSOCKET_EVENT_CONNECTED: {
+bool NeohubConnection::close(int timeoutMillis /* = -1 */) {
+    this->m_websocketClient.disconnect();
+    return true;
+}
+
+bool NeohubConnection::isConnected() {
+    return this->m_websocketClient.isConnected();
+}
+
+void NeohubConnection::webSocketEventHandler(WStype_t type, uint8_t * payload, size_t length, void *clientData) {
+    NeohubConnection* _this = (NeohubConnection *) clientData;
+    switch (type) {
+        case WStype_CONNECTED: {
             if (_this->m_on_connect) _this->m_on_connect();
             break;
         }
+        case WStype_DISCONNECTED: {
+            if (_this->m_on_disconnect) _this->m_on_disconnect();
+            break;
+        }
+        case WStype_TEXT:
+            if (_this->m_on_receive) _this->m_on_receive(String(payload, length));
+        break;
 
-        case WEBSOCKET_EVENT_DATA: {
-            if (_this->m_on_receive) {
-                esp_websocket_event_data_t *dataReceived = (esp_websocket_event_data_t *)event_data;
-                String data(dataReceived->data_ptr, dataReceived->data_len);
-                _this->m_on_receive(data);
-            }
+        case WStype_ERROR: {
+            if (_this->m_on_error) _this->m_on_error("Error event");
             break;
         }
 
-        case WEBSOCKET_EVENT_ERROR: {
-            if (_this->m_on_error) {
-                _this->m_on_error(NeohubConnectionResult::UnknownError, "???");
-            }
+        case WStype_FRAGMENT_TEXT_START:
+        case WStype_FRAGMENT:
+        case WStype_FRAGMENT_FIN: {
+            if (_this->m_on_error) _this->m_on_error("Fragment received - unhandled");
             break;
         }
 
-        case WEBSOCKET_EVENT_CLOSED: {
-            if (_this->m_on_close) _this->m_on_close();
+        // Binary data messages - we don't need them so we don't handle them
+        case WStype_BIN:
+        case WStype_FRAGMENT_BIN_START:
+        if (_this->m_on_error) _this->m_on_error("Binary data received - unhandled");
             break;
-        }
 
-        default: {
-            if (_this->m_on_error) _this->m_on_error(NeohubConnectionResult::UnknownEvent, String("Event ID: ") + String(event_id,10)) ;
+        // Messages we don't care for
+        case WStype_PING:
+        case WStype_PONG:
             break;
-        }
     }
 }
+
+TaskHandle_t NeohubConnection::m_loopTaskHandle = 0;
+std::unordered_set<NeohubConnection *> NeohubConnection::m_liveConnections;
+MyMutex NeohubConnection::m_loopMutex;
+
+void NeohubConnection::loopTask(void *parameter) {
+    for(;;) {
+        if (m_loopMutex.lock()) {
+            for (auto iterator = m_liveConnections.begin(); iterator != m_liveConnections.end(); ) {
+                NeohubConnection *connection = *iterator;
+                if (connection->m_deleted) {
+                    iterator = m_liveConnections.erase(iterator);
+                    delete connection;
+                }
+                else {
+                    connection->loop();
+                    iterator++;
+                }
+            }
+            m_loopMutex.unlock();
+        }
+        delay(10);
+    }
+}
+
+void NeohubConnection::addToLoopTask() {
+    if (m_loopMutex.lock()) {
+        m_liveConnections.insert(this);
+        m_loopMutex.unlock();
+    }
+}
+
+void NeohubConnection::ensureLoopTask() {
+    if (m_loopTaskHandle) return;
+    xTaskCreate(
+        NeohubConnection::loopTask, // Task function
+        "WebSocketLoop",            // Task name
+        4096,                       // Stack size (bytes)
+        NULL,                       // Parameter to pass
+        1,                          // Task priority
+        &m_loopTaskHandle           // Task handle
+    );
+}
+
+//     NeohubConnection* _this = (NeohubConnection *) arg;
+//     switch (event_id) {
+//         case WEBSOCKET_EVENT_DATA: {
+//             if (_this->m_on_receive) {
+//                 esp_websocket_event_data_t *dataReceived = (esp_websocket_event_data_t *)event_data;
+//                 Serial.printf(
+//                     "************************ Receive opcode = %d, data_len = %d, payload_len = %d, payload_offset=%d\n************************ %s\n",
+//                     dataReceived->op_code,
+//                     dataReceived->data_len,
+//                     dataReceived->payload_len,
+//                     dataReceived->payload_offset,
+//                     String(dataReceived->data_ptr, dataReceived->data_len).c_str()
+//                 );
+//                 // _this->m_responseText += String(dataReceived->data_ptr, dataReceived->data_len);
+//                 // if (dataReceived->payload_offset + dataReceived->data_len == dataReceived->payload_len) {
+//                 //     _this->m_on_receive(_this->m_responseText);
+//                 // }
+//             }
+//             break;
+            
+//         }
+
+//         case WEBSOCKET_EVENT_ERROR: {
+//             if (_this->m_on_error) {
+//                 // esp_websocket_event_data_t *errorData = (esp_websocket_event_data_t *) event_data;
+//                 // String message = String::format(
+//                 //     "TLS: 0x%x, stack: 0x%x, transport: 0x%x, errno: %d",
+//                 //     errorData->error_handle->esp_tls_last_error,
+//                 //     errorData->error_handle->esp_tls_stack_err,
+//                 //     errorData->error_handle->esp_transport_err,
+//                 //     errorData->error_handle->sock_errno
+//                 // );
+//                 _this->m_on_error("?"); 
+//                 // Current implementation of esp_websocket_client does not have error reporting
+//             }
+//             break;
+//         }
+
+//         case WEBSOCKET_EVENT_DISCONNECTED: {
+//             if (_this->m_on_disconnect) _this->m_on_disconnect("Unkown reason");
+//             break;
+//         }
+
+//         case WEBSOCKET_EVENT_CLOSED: {
+//             if (_this->m_on_close) _this->m_on_close();
+//             break;
+//         }
+
+//         default: {
+//             if (_this->m_on_error) _this->m_on_error(String("Unknown event, Event ID: ") + String(event_id,10)) ;
+//             break;
+//         }
+//     }
+// }
 
--- src/heatingControl/NeohubConnection.h
+++ src/heatingControl/NeohubConnection.h
@@ -1,31 +1,78 @@
-#include <Arduino.h>
-#include "esp_websocket_client.h"
+#ifndef __NEOHUB_CONNECTION_H
+#define __NEOHUB_CONNECTION_H
 
-enum class NeohubConnectionResult {
-    Ok = 0,
-    UnableToInitialiseClient,
-    UnknownEvent,
-    UnknownError
-};
+#include <Arduino.h>
+#include <WebSocketsClient.h>
+#include "MyMutex.h"
+#include <unordered_set>
 
 class NeohubConnection {
+    
 private:
-    String m_url;
-    esp_websocket_client_handle_t m_websocketClient;
-    void handleSocketConnected();   
-    static void websocketEventHandler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data);
-    std::function<void()> m_on_connect;
-    std::function<void(String)> m_on_receive;
-    std::function<void(NeohubConnectionResult error, String details)> m_on_error;
-    std::function<void()> m_on_close;
+    String m_host;
+    WebSocketsClient m_websocketClient;
+    static void webSocketEventHandler(WStype_t type, uint8_t * payload, size_t length, void *clientData);
+    std::function<void()> m_on_connect = nullptr;
+    std::function<void(String)> m_on_receive = nullptr;
+    std::function<void(String errorText)> m_on_error = nullptr;
+    std::function<void()> m_on_disconnect = nullptr;
+
+    String m_responseText;
+    bool m_deleted = false;
 
 public:
     NeohubConnection(const String &hubHost);
-    NeohubConnectionResult open();
-    bool close();
+private:
+    ~NeohubConnection();        // Private so nobody can delete an object from a callback
+                                // use NeohubConnection::remove() instead which will
+                                // delete the object in the next loop
+
+public:
+    bool open(int timeoutMillis = -1);
     bool send (String json, int timeoutMillis = -1);
+    bool close(int timeoutMillis = -1);
+    void finish() { m_deleted = true; };  // delayed deletion - will be deleted in next loop
+
+    // Set callback for when after the connection was established
     void onConnect(std::function<void()> func) { m_on_connect = func; };
+
+    // Set call back for when data is received
     void onReceive(std::function<void(String json)> func) { m_on_receive = func; }
-    void onError(std::function<void(NeohubConnectionResult error, String details)> func) { m_on_error = func; }
-    void onClose(std::function<void()> func) { m_on_close = func; }
+    void onError(std::function<void(String errorText)> func) { m_on_error = func; }
+
+    // Set callback on disconnection
+    // also called if establishing the connection in open() fails
+    void onDisconnect(std::function<void()> func) { m_on_disconnect = func; }
+
+    // check it this connection is (still) connected
+    bool isConnected();
+
+private:
+    // loop function for the connection - must be called regularlu
+    // originally intended for running in the main loop, but we
+    // use a loopTask for this
+    void loop() { 
+        Serial.println("LOOP");
+        m_websocketClient.loop(); 
+        Serial.println("DONE");
+    };
+
+    // The loop task which regularly executes the loop function for all connections
+    static TaskHandle_t m_loopTaskHandle;
+    static void ensureLoopTask();
+    static void loopTask(void *parameter);
+
+    // a set of all live connections for which loop() has to be called
+    static std::unordered_set<NeohubConnection *> m_liveConnections;
+
+    // A mutex preventinb concurrent access to the set with the live connections
+    static MyMutex m_loopMutex;
+
+    void addToLoopTask();
+
+
 };
+
+
+
+#endif
