552a2030a6da8cdeb1416c8e317b5719f73ae76c
Christof Edel <christof@edel.aero>
2025-12-20 10:32:34 +0000

    Monitor page tweaks

--- src/webserver/pages/MonitorPage.cpp
+++ src/webserver/pages/MonitorPage.cpp
@@ -22,7 +22,7 @@ void CMyWebServer::respondWithMonitorPage(AsyncWebServerRequest *request) {
       html.element("table", "class='field-table center-all-td'", [this, expertMode, &html] {
         html.print("<thead><tr>");
         html.print("<th style='border-bottom: none; min-width: 130px'></th><th style='width: 5em' class='gap-right'>Setpoint</th>");
-        html.print("<th style='width: 5em' class='gap-right'>Actual</th><th style='width: 5em' class='gap-right' >Diff.</th>");
+        html.print("<th style='width: 5em' class='gap-right'>Actual</th><th style='width: 5em' class='gap-right' >&Delta;t</th>");
         if (expertMode) {
           html.print("<th style='width: 5em' class='gap-right'>P</th><th style='width: 5em' >I</th>");
         }
