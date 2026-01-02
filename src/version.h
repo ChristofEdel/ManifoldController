466fa5a7513db23bd9d6ab91f5dd5892143441b6
Christof Edel <christof@edel.aero>
2026-01-02 13:59:25 +0000

    [DEV] Better /tasks page

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #ifndef __VERSION_H
 #define __VERSION_H
 
-const char* VERSION = "1.0.18";
+const char* VERSION = "1.0.20";
 
 #endif
--- src/webserver/pages/TasksPage.cpp
+++ src/webserver/pages/TasksPage.cpp
@@ -1,5 +1,32 @@
 #include "../MyWebServer.h"
 
+struct KnownTask {
+    const char* name;
+    const char* purpose;
+};
+
+KnownTask knownTasks[] = {
+    {"IDLE0",          "OS       | Idle    | Core 0 Idle Task"},
+    {"IDLE1",          "OS       | Idle    | Core 1 Idle Task"},
+    {"tiT",            "OS       | Idle    | Yet another idle task"},
+    {"mdns",           "OS       | Network | mDNS service task"},
+    {"async_tcp",      "AsyncTCP | Network | TCP event dispatcher and web server"},
+    {"sys_evt",        "OS       | Network | System network event dispatcher"},
+    {"wifi",           "OS       | Network | Wifi connecetion handler"},
+    {"websocket_task", "OS       | Network | UDP Websocket handler"},
+    {"esp_timer",      "OS       | Timers  | ESP precision timer dispatcher"},
+    {"Tmr Svc",        "OS       | Timers  | System timer service"},
+    {"ipc0",           "OS       |         | Inter-Processer Communication"},
+    {"loopTask",       "Arduino  |         | Arduino core task for setup and loop" },
+    {"ValveControl",   "APP      |         | Valve control loop"},
+    {"arduino_events", "Arduino  | Network | Arduino network event dispatcher"},
+    {"ipc1",           "OS       |         | Inter-Processer Communication"},
+    {"BgFileWriter",   "APP      |         | Backrgound (log) file writer"},
+    {"Watchdogs",      "APP      |         | Watchdogs"},
+    {"NeohubProxy",    "APP      |         | Neohub proxy polling loop"},
+    { nullptr,         "?        | ?       | ?" }
+};
+
 int taskCompare(const void *a, const void *b) {
     const TaskStatus_t *tsA = *(const TaskStatus_t **)a;
     const TaskStatus_t *tsB = *(const TaskStatus_t **)b;
@@ -29,13 +56,21 @@ void printTaskLine(AsyncResponseStream *response, TaskStatus_t& ts)
       default: stateStr = "??"; break;
     }
 
+    KnownTask *t = knownTasks;
+    while (t->name) {
+      if (strcmp(t->name, ts.pcTaskName) == 0) break;
+      t++;
+    }
+    const char *purpose = t->purpose;
+
     response->printf(
-      "%c %-18s|  %-3s | %4u |  %-3s  | %u\n",
+      "%c %-18s|  %-3s | %4u |  %-3s  | %8u | %s\n",
       ts.eCurrentState == eRunning ? '*' : ' ',
       ts.pcTaskName, affStr,
       ts.uxCurrentPriority,
       stateStr,
-      ts.usStackHighWaterMark
+      ts.usStackHighWaterMark,
+      purpose
     );
 
 }
@@ -57,16 +92,16 @@ void CMyWebServer::respondWithTaskList(AsyncWebServerRequest *request) {
   qsort(tsp, n, sizeof(TaskStatus_t *), taskCompare);
 
   // Print table
-  response->println("Task                | core | prio | state | stack HW");
-  response->println("--------------------+------+------+-------+---------");
+  response->println("Task                | core | prio | state | stack HW | Owner    | Purpose | Description");
+  response->println("--------------------+------+------+-------+----------+----------+---------+------------------------------");
 
   for (UBaseType_t i = 0; i < n; ++i) if (tsp[i]->xCoreID == 0) printTaskLine(response, *tsp[i]);
-  response->println("--------------------+------+------+-------+---------");
+  response->println("--------------------+------+------+-------+----------+----------+---------+------------------------------");
   for (UBaseType_t i = 0; i < n; ++i) if (tsp[i]->xCoreID == 1) printTaskLine(response, *tsp[i]);
-  response->println("--------------------+------+------+-------+---------");
+  response->println("--------------------+------+------+-------+----------+----------+---------+------------------------------");
   for (UBaseType_t i = 0; i < n; ++i) if (tsp[i]->xCoreID >1) printTaskLine(response, *tsp[i]);
 
-  response->println("--------------------+------+------+-------+---------");
+  response->println("--------------------+------+------+-------+----------+----------+---------+------------------------------");
   free(ts);
   request->send(response);
 }
